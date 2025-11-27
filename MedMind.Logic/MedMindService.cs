ce3c14a7d7a457835a4af9b22706d669c7952f55
shawn miner <shawnmin13@gmail.com>
2025-11-26 19:28:18 -0700

    MedMindService class and functions complete

---
+++ MedMind.Logic/MedMindService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+namespace MedMind.Logic;
+public class MedMindService
+{
+    public List<Medication> Medications { get; }
+    public List<Appointment> Appointments { get; }
+
+    public MedMindService()
+    {
+        Medications = new();
+        Appointments = new();
+    }
+
+    public void AddMedication(Medication med) => Medications.Add(med);
+
+    public List<Medication> GetMedications() => Medications;
+
+    public void AddAppointment(Appointment appt) => Appointments.Add(appt);
+
+    public List<Appointment> GetAppointmentsForDate(DateTime time) => Appointments.FindAll(a => a.AppointmentTime == time);
+}
