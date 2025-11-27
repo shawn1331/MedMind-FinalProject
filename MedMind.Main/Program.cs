b1b54f2c9ed0d2d8da785a3a9310b0fdb710e75d
shawn miner <shawnmin13@gmail.com>
2025-11-26 18:00:36 -0700

    placeholder message added ready to start branches, features, and functionality

--- MedMind.Main/Program.cs
+++ MedMind.Main/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Shawn Miner MedMind Final Project Intro. To Software Engineering
+
+Console.WriteLine("MedMind");
